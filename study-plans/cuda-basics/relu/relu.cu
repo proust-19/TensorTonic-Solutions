cfd784f847e43e08b373339d70b26d3b4caa4de0
Purshotam Kumar <proustbritish38@gmail.com>
2026-07-05 23:09:42 +0530

    Solve relu

---
+++ study-plans/cuda-basics/relu/relu.cu
@@ -0,0 +1,18 @@
+#include <cuda_runtime.h>
+
+__global__ void relu_kernel(const float* input, float* output, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < N) {
+        float x = input[idx];
+        output[idx] = (x > 0.0f) ? x : 0.0f;
+    }
+    
+}
+
+extern "C" void solve(const float* input, float* output, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    relu_kernel<<<blocks, threads>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
